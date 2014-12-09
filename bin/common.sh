0c265276cf30d4fa7d51f8e6f0b3d1672c54f608
Hunter Loftis <hunter@hunterloftis.com>
2014-12-09 11:44:06 -0800

    update

--- bin/common.sh
+++ bin/common.sh
@@ -69,7 +69,7 @@ quote() {
   echo ""
   local url="http://www.iheartquotes.com/api/v1/random?source=prog_style+esr+osp_rules+esr&max_lines=3&show_permalink=false&show_source=false"
   local text=$(curl --max-time 3 $url 2> /dev/null)
-  local withQuotes=${text/&quot;/"}
+  local withQuotes=${text/&quot;/\"}
   local withAmps=${withQuotes/&amp;/&}
   echo $withAmps
 }
