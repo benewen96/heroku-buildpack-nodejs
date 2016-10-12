5cd2ce38895b04265a68454b53221839f472556f
Hunter Loftis <hunter@hunterloftis.com>
2016-10-12 16:51:40 -0400

    better warning output

--- lib/failure.sh
+++ lib/failure.sh
@@ -2,21 +2,22 @@ warnings=$(mktemp -t heroku-buildpack-nodejs-XXXX)
 
 failure_message() {
   local warn="$(cat $warnings)"
-  echo ""
-  echo "We're sorry this build is failing! You can troubleshoot common issues here:"
-  echo "https://devcenter.heroku.com/articles/troubleshooting-node-deploys"
-  echo ""
+  echo "We're sorry this build is failing!"
   if [ "$warn" != "" ]; then
-    echo "Some possible problems:"
-    echo ""
-    echo "$warn"
-  else
+    echo "We'd recommend starting debugging with the issues listed below."
     echo "If you're stuck, please submit a ticket so we can help:"
     echo "https://help.heroku.com/"
+    echo ""
+    echo "💜 Heroku"
+    echo "$warn"
+  else
+    echo "You can troubleshoot common issues here:"
+    echo "https://devcenter.heroku.com/articles/troubleshooting-node-deploys"
+    echo "If you're stuck, please submit a ticket so we can help:"
+    echo "https://help.heroku.com/"
+    echo ""
+    echo "💜 Heroku"
   fi
-  echo ""
-  echo "Love,"
-  echo "Heroku"
   echo ""
 }
 
@@ -28,44 +29,32 @@ fail_invalid_package_json() {
   fi
 }
 
-warning() {
-  local tip=${1:-}
-  local url=${2:-https://devcenter.heroku.com/articles/nodejs-support}
-  echo "- $tip" >> $warnings
-  echo "  $url" >> $warnings
-  echo "" >> $warnings
-}
-
-warn() {
-  local tip=${1:-}
-  local url=${2:-https://devcenter.heroku.com/articles/nodejs-support}
-  echo " !     $tip" || true
-  echo "       $url" || true
-  echo ""
+add_warning() {
+  warn "$1" "$2" >> $warnings
 }
 
 warn_node_engine() {
   local node_engine=${1:-}
   if [ "$node_engine" == "" ]; then
-    warning "Node version not specified in package.json" "https://devcenter.heroku.com/articles/nodejs-support#specifying-a-node-js-version"
+    add_warning "Node version not specified in package.json" "https://devcenter.heroku.com/articles/nodejs-support#specifying-a-node-js-version"
   elif [ "$node_engine" == "*" ]; then
-    warning "Dangerous semver range (*) in engines.node" "https://devcenter.heroku.com/articles/nodejs-support#specifying-a-node-js-version"
+    add_warning "Dangerous semver range (*) in engines.node" "https://devcenter.heroku.com/articles/nodejs-support#specifying-a-node-js-version"
   elif [ ${node_engine:0:1} == ">" ]; then
-    warning "Dangerous semver range (>) in engines.node" "https://devcenter.heroku.com/articles/nodejs-support#specifying-a-node-js-version"
+    add_warning "Dangerous semver range (>) in engines.node" "https://devcenter.heroku.com/articles/nodejs-support#specifying-a-node-js-version"
   fi
 }
 
 warn_prebuilt_modules() {
   local build_dir=${1:-}
   if [ -e "$build_dir/node_modules" ]; then
-    warning "node_modules checked into source control" "https://blog.heroku.com/node-habits-2016#9-only-git-the-important-bits"
+    add_warning "node_modules checked into source control" "https://blog.heroku.com/node-habits-2016#9-only-git-the-important-bits"
   fi
 }
 
 warn_missing_package_json() {
   local build_dir=${1:-}
   if ! [ -e "$build_dir/package.json" ]; then
-    warning "No package.json found"
+    add_warning "No package.json found"
   fi
 }
 
@@ -73,38 +62,38 @@ warn_old_npm() {
   local npm_version="$(npm --version)"
   if [ "${npm_version:0:1}" -lt "2" ]; then
     local latest_npm="$(curl --silent --get --retry 5 --retry-max-time 15 https://semver.herokuapp.com/npm/stable)"
-    warning "This version of npm ($npm_version) has several known issues - consider upgrading to the latest release ($latest_npm)" "https://devcenter.heroku.com/articles/nodejs-support#specifying-an-npm-version"
+    add_warning "This version of npm ($npm_version) has several known issues - consider upgrading to the latest release ($latest_npm)" "https://devcenter.heroku.com/articles/nodejs-support#specifying-an-npm-version"
   fi
 }
 
 warn_untracked_dependencies() {
   local log_file="$1"
   if grep -qi 'gulp: not found' "$log_file" || grep -qi 'gulp: command not found' "$log_file"; then
-    warning "Gulp may not be tracked in package.json" "https://devcenter.heroku.com/articles/troubleshooting-node-deploys#ensure-you-aren-t-relying-on-untracked-dependencies"
+    add_warning "Gulp may not be tracked in package.json" "https://devcenter.heroku.com/articles/troubleshooting-node-deploys#ensure-you-aren-t-relying-on-untracked-dependencies"
   fi
   if grep -qi 'grunt: not found' "$log_file" || grep -qi 'grunt: command not found' "$log_file"; then
-    warning "Grunt may not be tracked in package.json" "https://devcenter.heroku.com/articles/troubleshooting-node-deploys#ensure-you-aren-t-relying-on-untracked-dependencies"
+    add_warning "Grunt may not be tracked in package.json" "https://devcenter.heroku.com/articles/troubleshooting-node-deploys#ensure-you-aren-t-relying-on-untracked-dependencies"
   fi
   if grep -qi 'bower: not found' "$log_file" || grep -qi 'bower: command not found' "$log_file"; then
-    warning "Bower may not be tracked in package.json" "https://devcenter.heroku.com/articles/troubleshooting-node-deploys#ensure-you-aren-t-relying-on-untracked-dependencies"
+    add_warning "Bower may not be tracked in package.json" "https://devcenter.heroku.com/articles/troubleshooting-node-deploys#ensure-you-aren-t-relying-on-untracked-dependencies"
   fi
 }
 
 warn_angular_resolution() {
   local log_file="$1"
   if grep -qi 'Unable to find suitable version for angular' "$log_file"; then
-    warning "Bower may need a resolution hint for angular" "https://github.com/bower/bower/issues/1746"
+    add_warning "Bower may need a resolution hint for angular" "https://github.com/bower/bower/issues/1746"
   fi
 }
 
 warn_missing_devdeps() {
   local log_file="$1"
   if grep -qi 'cannot find module' "$log_file"; then
-    warning "A module may be missing from 'dependencies' in package.json" "https://devcenter.heroku.com/articles/troubleshooting-node-deploys#ensure-you-aren-t-relying-on-untracked-dependencies"
+    add_warning "A module may be missing from 'dependencies' in package.json" "https://devcenter.heroku.com/articles/troubleshooting-node-deploys#ensure-you-aren-t-relying-on-untracked-dependencies"
     if [ "$NPM_CONFIG_PRODUCTION" == "true" ]; then
       local devDeps=$(read_json "$BUILD_DIR/package.json" ".devDependencies")
       if [ "$devDeps" != "" ]; then
-        warning "This module may be specified in 'devDependencies' instead of 'dependencies'" "https://devcenter.heroku.com/articles/nodejs-support#devdependencies"
+        add_warning "This module may be specified in 'devDependencies' instead of 'dependencies'" "https://devcenter.heroku.com/articles/nodejs-support#devdependencies"
       fi
     fi
   fi
@@ -125,7 +114,7 @@ warn_no_start() {
 warn_econnreset() {
   local log_file="$1"
   if grep -qi 'econnreset' "$log_file"; then
-    warning "ECONNRESET issues may be related to npm versions" "https://github.com/npm/registry/issues/10#issuecomment-217141066"
+    add_warning "ECONNRESET issues may be related to npm versions" "https://github.com/npm/registry/issues/10#issuecomment-217141066"
   fi
 }
 
--- lib/output.sh
+++ lib/output.sh
@@ -1,5 +1,5 @@
 info() {
-  echo "       $*" || true
+  echo "$*" || true
 }
 
 # format output and send a copy to the log
@@ -18,12 +18,15 @@ header() {
   echo -e "\033[96m\033[1m\033[40m=== $*\033[0m" || true
 }
 
-warning() {
+warn() {
+  local message=${1:-}
+  local suggestion=${2:-https://devcenter.heroku.com/articles/nodejs-support}
   echo "" || true
-  echo -e "\033[93m\033[1m\033[40m! $*\033[0m" >&2 || true
+  echo -e "\033[93m\033[1m\033[40m! $message\033[0m" || true
+  echo "$suggestion"
 }
 
 error() {
   echo "" || true
-  echo -e "\033[91m\033[1m\033[40m! $*\033[0m" >&2 || true
+  echo -e "\033[91m\033[1m\033[40m! $*\033[0m" || true
 }
