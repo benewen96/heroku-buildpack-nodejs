91d362f59500f2076c702229ba04a01e11c1e0df
Hunter Loftis <hunter@hunterloftis.com>
2016-10-18 16:16:06 -0400

    update with === and =!=

--- lib/output.sh
+++ lib/output.sh
@@ -15,18 +15,18 @@ output() {
 
 header() {
   echo "" || true
-  echo -e "\033[96m\033[1m\033[40mNode: $*\033[0m" || true
+  echo -e "\033[96m\033[1m\033[40m=== $*\033[0m" || true
 }
 
 warn() {
   local message=${1:-}
   local suggestion=${2:-https://devcenter.heroku.com/articles/nodejs-support}
   echo "" || true
-  echo -e "\033[93m\033[1m\033[40m! $message\033[0m" || true
+  echo -e "\033[93m\033[1m\033[40m=!= $message\033[0m" || true
   echo "$suggestion"
 }
 
 error() {
   echo "" || true
-  echo -e "\033[91m\033[1m\033[40m! $*\033[0m" || true
+  echo -e "\033[91m\033[1m\033[40m=!= $*\033[0m" || true
 }
