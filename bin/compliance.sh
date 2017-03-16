ae9b4acf0fea28c1399015d872e25db273b5ab70
Ben Ewen <ewen_ben@network.lilly.com>
2017-03-16 10:10:53 +0000

    remove master check

--- bin/compliance.sh
+++ bin/compliance.sh
@@ -5,15 +5,9 @@ export_env_dir $env_dir
 
 
 check_compliance() {
-  echo "Compliance checks echo!"
-  echo $HEROKU_TEST_RUN_BRANCH
-  # if we're on the master branch, then run production tests (i.e. codecheck)
-  if [ "$HEROKU_TEST_RUN_BRANCH" == "master" ]
-  then
-      echo "Production branch detected, running long tests..."
-      # Script to run codecheck
-      rm -rf .codecheck
-      git clone --depth 1 https://${GITHUB_USERNAME}:${API_TOKEN}@github.com/EliLillyCo/CIRR_HerokuCI.git .codecheck
-      .codecheck/runtests | indent
-  fi
+  echo "Production branch detected, running long tests..."
+  # Script to run codecheck
+  rm -rf .codecheck
+  git clone --depth 1 https://${GITHUB_USERNAME}:${API_TOKEN}@github.com/EliLillyCo/CIRR_HerokuCI.git .codecheck
+  .codecheck/runtests | indent
 }
