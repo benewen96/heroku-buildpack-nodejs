9cc3ea6fc0ca308a60c143bbf213997003073ee5
Ben Ewen <ewen_ben@network.lilly.com>
2017-03-16 10:32:49 +0000

    indent

--- bin/compliance.sh
+++ bin/compliance.sh
@@ -5,12 +5,11 @@ export_env_dir $env_dir
 
 
 check_compliance() {
-  status "Production branch detected, running long tests..."
   # Script to run codecheck
   cd $build_dir
   rm -rf .codecheck
-  git clone --depth 1 https://${GITHUB_USERNAME}:${API_TOKEN}@github.com/EliLillyCo/CIRR_HerokuCI.git .codecheck
+  git clone --depth 1 https://${GITHUB_USERNAME}:${API_TOKEN}@github.com/EliLillyCo/CIRR_HerokuCI.git .codecheck | indent
   status "Start Cirrus codecheck process: `date`"
-  java -jar .codecheck/jar/herokuci.jar
+  java -jar .codecheck/jar/herokuci.jar | indent
   status "End Cirrus codecheck process: `date`"
 }
