eb2aeef27801d8726ffd7686e91b1f35476a9868
Ben Ewen <ewen_ben@network.lilly.com>
2017-03-16 10:12:03 +0000

    ensure in build_dir

--- bin/compliance.sh
+++ bin/compliance.sh
@@ -7,6 +7,7 @@ export_env_dir $env_dir
 check_compliance() {
   echo "Production branch detected, running long tests..."
   # Script to run codecheck
+  cd $build_dir
   rm -rf .codecheck
   git clone --depth 1 https://${GITHUB_USERNAME}:${API_TOKEN}@github.com/EliLillyCo/CIRR_HerokuCI.git .codecheck
   .codecheck/runtests | indent
